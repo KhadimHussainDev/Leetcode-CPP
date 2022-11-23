45db918b8ebfd4c42bb4eae296bd2087e547159b
Khadim92 <imkhadim1@gmail.com>
2022-11-23 07:16:33 +0500

    more task added

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -5,11 +5,9 @@ public:
     {
         int n = s.length(), m = t.length();
         int j = 0;
-
         for (int i = 0; i < m and j < n; i++)
             if (s[j] == t[i])
                 j++;
-
         return (j == n);
     }
 };
