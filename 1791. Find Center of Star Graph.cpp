dcef2e8f97ea6760eac23d24f433dcc8b4cfa880
Khadim92 <imkhadim1@gmail.com>
2022-12-03 16:26:48 +0500

    more tasks solved

---
+++ 1791. Find Center of Star Graph.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int size=edges.size()+1;
+        vector<int>degrees(size+1);
+        int star=0;int largest=0;
+        for(auto x:edges){
+            int a=++degrees[x[0]];
+            int b=++degrees[x[1]];
+            if(largest<a){
+                largest=a;
+                star=x[0];
+                if(a==2)
+                    return star;
+            }
+            if(largest<b){
+                largest=b;
+                star=x[1];
+                if(a==2)
+                    return star;
+            }
+        }
+        return star;
+    }
+};
