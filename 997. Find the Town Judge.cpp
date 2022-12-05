ab01d4eb3125148bc55b659c5431f6ea528aeb0d
Khadim92 <imkhadim1@gmail.com>
2022-12-05 16:27:52 +0500

    more tasks solved

---
+++ 997. Find the Town Judge.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.size()==0&&n==1){
+            return n;
+        }
+        unordered_map<int,int>map;
+        for(auto x:trust){
+            
+            map[x[1]]++;
+
+        }
+        int largest=0,judge=-1;
+        for(auto a:map){
+            if(a.second>largest){
+                largest=a.second;
+                judge=a.first;
+            }
+        }
+        for(auto a:trust){
+            if(judge==a[0])
+                return -1;
+        }
+        int b=map[judge]++;
+        for(auto a:map){
+            if(b==a.second)
+                return -1;
+        }
+        if(b>=n-1)
+        return judge;
+        return -1;
+    }
+};
