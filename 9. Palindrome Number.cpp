e8d07e3250bcf17be91277b52d733a50414ed3bb
Khadim92 <imkhadim1@gmail.com>
2022-12-12 05:39:53 +0500

    more tasks solved

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        stack <int> vec;
+        int y=x;
+        while(y>0){
+           int i=y%10;
+           vec.push(i);
+           y=y/10;
+          
+        }
+         while(!vec.empty()){
+           int j=vec.top();
+           vec.pop();
+            if (x%10!=j){
+               return false;
+                break;
+            }
+                x=x/10;
+           }
+           return true;
+    }
+};
