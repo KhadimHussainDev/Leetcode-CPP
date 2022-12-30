a413e45f2b8053f9f3aca0791ca41a5d6becacdd
Khadim Hussain <imkhadim1@gmail.com>
2022-12-30 18:41:36 +0500

    not changed

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             for(int j=i+1 ; j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                   vec.push_back(i);
+                    vec.push_back(i);
                     vec.push_back(j);
                     return vec;
                 }
