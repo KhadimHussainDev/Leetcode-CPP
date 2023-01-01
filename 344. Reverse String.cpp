afe3c986318887dc0a58a7fa179735b7de7a54c0
Khadim92 <imkhadim1@gmail.com>
2023-01-01 18:15:26 +0500

    improved

--- 344. Reverse String.cpp
+++ 344. Reverse String.cpp
@@ -6,7 +6,9 @@ public:
         int i = 0, j = s.size() - 1;
         while (i < j)
         {
-            swap(s[i], s[j]);
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
